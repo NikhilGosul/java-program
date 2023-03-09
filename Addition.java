be161bd5eeabc0e95f6b75281d28cb5a8e75ba27
NikhilGosul <127389869+NikhilGosul@users.noreply.github.com>
2023-03-09 14:51:25 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Addition {
+  public static void main(String[] args) {
+    int num1 = 99, num2 = 44,sum=0;
+    sum = num1+num2;
+    System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+  }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main
+{
+ public static void main(String[] args)
+{
+ char letter ='\u051';
+ System.out.println(letter);
+ }
+}
